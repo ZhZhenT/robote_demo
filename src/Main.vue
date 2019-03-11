ff81f08054701375e23963c5013d2789b996e695
ZhanZhenT <849723358@qq.com>
2019-03-11 15:20:50 +0800

    first commit

---
+++ src/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="less">
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+}
+
+</style>
+<script>
+export default {
+  name: 'app',
+  mounted () {
+    console.log(2222)
+  }
+}
+</script>
